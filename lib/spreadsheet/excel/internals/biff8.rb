226373bf5a07400cd4075c161432d6068bb5cffb
Tim Linquist <tim.linquist@livingsocial.com>
2015-06-28 23:31:19 -0700

    Improve lookup to constant time
    The lookup calling super for every fetch where the key does not exist is
    very slow. Dramatic difference in cpu profile by copying the hash into
    each module and avoiding the method lookup to Internals module

--- lib/spreadsheet/excel/internals/biff8.rb
+++ lib/spreadsheet/excel/internals/biff8.rb
@@ -1,19 +1,20 @@
 module Spreadsheet
   module Excel
     module Internals
-##
-# Binary Formats and other configurations internal to Biff8. This Module is
-# likely to be expanded as Support for older Versions of Excel grows and more
-# Binary formats are moved here for disambiguation.
-module Biff8
-  BINARY_FORMATS = {
-    :bof        => 'v4V2',
-    :dimensions => 'V2v2x2',
-  }
-  def binfmt key # :nodoc:
-    BINARY_FORMATS.fetch key do super end
-  end
-end
+      ##
+      # Binary Formats and other configurations internal to Biff8. This Module is
+      # likely to be expanded as Support for older Versions of Excel grows and more
+      # Binary formats are moved here for disambiguation.
+      module Biff8
+        BINARY_FORMATS = {
+          :bof        => 'v4V2',
+          :dimensions => 'V2v2x2',
+        }.merge(Spreadsheet::Excel::Internals::BINARY_FORMATS)
+
+        def binfmt(key)
+          BINARY_FORMATS[key]
+        end
+      end
     end
   end
 end
